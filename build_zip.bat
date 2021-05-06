03c6a623c1ff5835be209d02145022d4cc55ab57
Steven Barnes <Steven.Barnes@bakerhughes.com>
2021-05-06 16:34:50 +0100

    Added readme & license to build

--- build_zip.bat
+++ build_zip.bat
@@ -1,2 +1,2 @@
 py gen_build_info.py
-py -m zipfile -c ..\BH_Pip.py __main__.py bh_pip.py bh_pip_py2.py build_info.py defaults.py PySocks-1.7.1-py27-none-any.whl PySocks-1.7.1-py3-none-any.whl
+py -m zipfile -c ..\BH_Pip.py __main__.py bh_pip.py bh_pip_py2.py build_info.py defaults.py PySocks-1.7.1-py27-none-any.whl PySocks-1.7.1-py3-none-any.whl README.md BH-INTERNAL-COPY-LEFT-LICENSE.md
